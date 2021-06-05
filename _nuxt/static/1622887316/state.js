ec384da4e7756019bdb45a46792cc6e3e5e8b0f8
Taufiq Pratama <taufiq.el.pratama@gmail.com>
2021-06-05 19:02:01 +0900

    db9fc64

---
+++ _nuxt/static/1622887316/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgithub-page\u002F_nuxt\u002Fstatic\u002F1622887316",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgithub-page\u002F",assetsPath:"\u002Fgithub-page\u002F_nuxt\u002F",cdnURL:a}}}}(null));
